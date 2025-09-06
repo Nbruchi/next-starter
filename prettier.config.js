b45cb30eb31efc634389742078a2eff7a91190f0
Nbruchi <nkundabagenzibruce@gmail.com>
2025-09-06 13:07:41 +0200

    style(entire app): i installed prettier and eslint and linted my project
    I installed eslint and prettier. Configured them to work together and install husky to streamline
    commits

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSameLine: true,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: "always",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
